92b625655c3471fe8277539ec3268ecfbcd20a68
Julius <yulii.vedkal@nure.ua>
2021-04-11 01:36:39 +0300

    [add][update] - add log4net

---
+++ BrainstormSessions/Services/LoggerManager.cs
@@ -0,0 +1,26 @@
+﻿using BrainstormSessions.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BrainstormSessions.Services
+{
+    public static class LoggerManager
+    {
+        private static Dictionary<Type, ILogger> _loggers;
+
+        static LoggerManager()
+        {
+            _loggers = new Dictionary<Type, ILogger>();
+        }
+
+        public static ILogger GetLogger<TModel>()
+        {
+            if (!_loggers.ContainsKey(typeof(TModel)))
+            {
+                _loggers.Add(typeof(TModel), new Logger<TModel>());
+            }
+
+            return _loggers[typeof(TModel)];
+        }
+    }
+}
